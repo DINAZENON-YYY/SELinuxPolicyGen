79ea69256663b6a651b109ce7859de8ec5c50d1c
DINAZENON-YYY <82509401+DINAZENON-YYY@users.noreply.github.com>
2024-05-19 15:53:16 +0800

    Add files via upload

--- execFilePolicyGen.py
+++ execFilePolicyGen.py
@@ -4,5 +4,42 @@
 
 """
 
-def InitAppExcFilePolicy():
-    pass
+from dataStruct import AppPolicyInfo
+from seProcess import findInDefault
+import os
+
+def InitAppExcFilePolicy(app_policy_info : AppPolicyInfo):
+    if not os.path.exists(app_policy_info.path):
+        return False
+    # 转换为绝对路径
+    absolute_path = os.path.abspath(app_policy_info.path)
+    # 判断是否为可执行文件
+    if os.access(absolute_path, os.X_OK):
+        type_default, ok = findInDefault(app_policy_info.path)
+        # 判断是否有默认type
+        if ok:
+            app_policy_info.policy_exec_file += f"gen_context(system_u: object_r: {type_default}, s0)\n"
+        else:
+            app_policy_info.policy_exec_file += f"gen_context(system_u: object_r: {app_policy_info.security_exec_label}, s0)\n"
+    return  True
+
+"""
+测试
+def InitAppExcFilePolicy1(app_policy_info : AppPolicyInfo):
+    type_default, ok = findInDefault(app_policy_info.path)
+    # 判断是否有默认type
+    if ok:
+        app_policy_info.policy_exec_file += f"gen_context(system_u: object_r: {type_default}, s0)"
+    else:
+        app_policy_info.policy_exec_file += f"gen_context(system_u: object_r: {app_policy_info.security_exec_label}, s0)"
+    return True
+
+test = AppPolicyInfo(name= "test")
+test.path = "/var/log/messsages"
+test.security_exec_label = "test_exec_t"
+
+ok = InitAppExcFilePolicy1(test)
+if ok:
+    print(test.policy_exec_file)
+    
+"""
--- seProcess.py
+++ seProcess.py
@@ -6,7 +6,10 @@
 
 
 from dataStruct import AppPolicyInfo
+import json
+import re
 
+type_default_path = r"save_default"
 def simpleGenerateSecurityLabel(appPolicyInfo: AppPolicyInfo):
     appPolicyInfo.security_label = appPolicyInfo.name + "_t"
 
@@ -18,9 +21,30 @@ def simpleGenerateDataSecurityLabel(appPolicyInfo: AppPolicyInfo):
 
 """寻找默认type也可以判断是否为公有还是私有，待完善"""
 def findInDefault(fileName : str):
-    fileVector = []
-    return None, False
-    if fileName in fileVector:
-        return fileVector[fileName], True
-    else:
+    # 加载json文件
+    with open(type_default_path, 'r', encoding="UTF-8") as f:
+        file_data = json.load(f)
+    # 分割，寻找默认簇
+    file_name_split = fileName.split('/')
+    file_class = file_name_split[1]
+    # 判断是否为默认type
+    if file_class not in file_data:
         return None, False
+    file_type_set = file_data[file_class]
+    res_type = None
+    res_ok = False
+    # 通过正则表达式匹配默认type
+    for k,v in file_type_set.items():
+        pattern = k
+        match = re.search(pattern, fileName)
+        if match:
+            res_type = v
+            res_ok = True
+            break
+    return res_type, res_ok
+
+"""
+测试
+type, ok = findInDefault("/var/log/messsages")
+print(type)
+"""
